d5226b698ddb5059dafe6e50559fb261e447dca9
harshitrao-cslr <hrao@completesolar.com>
2025-03-27 13:05:27 +0530

    docker changes

--- GoalsApi/main.py
+++ GoalsApi/main.py
@@ -3,12 +3,21 @@ from controllers.goals import Goals, router
 from database import engine
 from models.models import Base
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+
 # Initialize FastAPI app
 app = FastAPI(
     title="Goals API",
     description="This is a custom API for my project, with Swagger integration.",
     version="1.0.0"
 )
+
+@app.get("/")
+async def redirect_root():
+    return RedirectResponse(url="/goals")
+
+app.mount("/goals", StaticFiles(directory="/app/static", html=True), name="static")
 
 origins = [
     "http://localhost",
