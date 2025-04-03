531f2394acbd7160acfc563a1f8fd382e944e6e4
harshitrao-cslr <hrao@completesolar.com>
2025-04-03 12:32:46 +0530

    local url changes

--- Goals-UI/src/environments/environment.ts
+++ Goals-UI/src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
-    baseURL: 'https://goals-dev.completesolar.com/api',
+    baseURL: 'http://localhost:7000/api',
     production: false,
-    serviceURL: "https://goals-dev.completesolar.com",
+    serviceURL: "http://localhost:4200",
     config: {
         auth: {
             "clientId": "1a3f14be-e3aa-453c-ac73-190faf1098ce",
             "authority": "https://login.microsoftonline.com/43754c3b-6e44-4fc9-8b0a-304ac51f69a1",
             "validateAuthority": true,
-            "redirectUri": "https://goals-dev.completesolar.com/goals",
-            "postLogoutRedirectUri": "https://goals-dev.completesolar.com",
+            "redirectUri": "http://localhost:4200/goals",
+            "postLogoutRedirectUri": "http://localhost:4200",
             "navigateToLoginRequestUrl": true
         },
         cache: {
@@ -17,7 +17,7 @@ export const environment = {
 
         resources: {
             "demoApi": {
-                "resourceUri": "https://goals-dev.completesolar.com", // example "http://localhost:4013"
+                "resourceUri": "http://localhost:4200", // example "http://localhost:4013"
                 "resourceScope": "api://1a3f14be-e3aa-453c-ac73-190faf1098ce/User.read", // here use the client id of the Web API you registered
             }
         },
