0342ad1d2964f6416a95c5af5c31bb62985dbd45
NandhiniMahadevan <Nandhini.Mahadevan@blueravensolar.com>
2025-05-05 16:58:19 +0530

    filter issue fix

--- frontend/src/app/master-components/delinquent/delinquent.component.ts
+++ frontend/src/app/master-components/delinquent/delinquent.component.ts
@@ -103,7 +103,14 @@ export class DelinquentComponent {
 
   async updateD(item: any) {
     const isChanged = await this.isObjectChanged(item, this.editingItem);
-    if (!this.editingItem && isChanged) return;
+   if (isChanged === false) {
+     this.messageService.add({
+       severity: 'info',
+       summary: 'No Changes Detected',
+     });
+     this.cancelEdit();
+     return;
+   }
     this.goalsService.updateD(this.editingItem).subscribe({
       next: (response: any) => {
         if (response && response.id) {
